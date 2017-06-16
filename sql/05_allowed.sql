c76847e41a1ef704ca9fd3f94229316b05194afc
Jeremy Finzel <jfinzel@enova.com>
2017-06-16 11:52:35 -0500

    Add tests for all cases in filtered schemas

--- sql/05_allowed.sql
+++ sql/05_allowed.sql
@@ -1,5 +1,9 @@
 SET ROLE test_pgl_ddl_deploy;
 SET client_min_messages TO warning;
+
+/***
+In default schema
+**/
 CREATE TABLE foo(id serial primary key);
 SELECT * FROM check_rep_tables;
 SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
@@ -10,22 +14,6 @@ SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY i
 INSERT INTO foo (bla) VALUES (1),(2),(3);
 
 DROP TABLE foo CASCADE;
-SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
-
-CREATE SCHEMA foobar;
-SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
-
-CREATE TABLE foobar.foo(id serial primary key);
-SELECT * FROM check_rep_tables;
-SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
-
-ALTER TABLE foobar.foo ADD COLUMN bla TEXT;
-SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
-
-INSERT INTO foobar.foo (bla) VALUES (1),(2),(3);
-
-DROP SCHEMA foobar CASCADE;
-SELECT * FROM check_rep_tables;
 SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
 
 CREATE FUNCTION foo() RETURNS INT AS
@@ -58,6 +46,57 @@ ALTER SEQUENCE foo RESTART;
 SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
 
 DROP SEQUENCE foo;
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+CREATE SCHEMA foobar;
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+CREATE TABLE foobar.foo(id serial primary key);
+SELECT * FROM check_rep_tables;
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+ALTER TABLE foobar.foo ADD COLUMN bla TEXT;
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+INSERT INTO foobar.foo (bla) VALUES (1),(2),(3);
+
+DROP TABLE foobar.foo CASCADE;
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+CREATE FUNCTION foobar.foo() RETURNS INT AS
+$BODY$
+SELECT 1;
+$BODY$
+LANGUAGE SQL STABLE;
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+ALTER FUNCTION foobar.foo() OWNER TO current_user;
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+DROP FUNCTION foobar.foo();
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+CREATE VIEW foobar.fooview AS
+SELECT 1 AS myfield;
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+ALTER VIEW foobar.fooview RENAME TO barview;
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+DROP VIEW foobar.barview;
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+CREATE SEQUENCE foobar.foo;
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+ALTER SEQUENCE foobar.foo RESTART;
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+DROP SEQUENCE foobar.foo;
+SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
+
+DROP SCHEMA foobar CASCADE;
+SELECT * FROM check_rep_tables;
 SELECT set_name, ddl_sql_raw, ddl_sql_sent FROM pgl_ddl_deploy.events ORDER BY id DESC LIMIT 10;
 
 SELECT * FROM pgl_ddl_deploy.unhandled;
